c828c30e64558a8745b2b13c0a7b531d6a0fd5a7
Cefas Natanael da Silva Gonçalves de Melo <cefas.natanael@academico.ifrn.edu.br>
2023-04-14 15:46:07 -0300

    envio 5

--- beardless warrior/Assets/scripts/Barbarian.cs
+++ beardless warrior/Assets/scripts/Barbarian.cs
@@ -12,14 +12,19 @@ public class Barbarian : MonoBehaviour
     }
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
-        transform.position = transform.position + movement * speed * Time.deltaTime;
-
+        Andar();
         if (Input.GetKeyDown(KeyCode.Z))
         {
             attacking = true;   
             
             
         }
+    }
+
+    public void Andar()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        transform.position = transform.position + movement * speed * Time.deltaTime;
+
     }
 }
